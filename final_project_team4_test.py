b4df1956b26521347264a82a2f9b111bf087aa9e
Ei Tanaka <eieio.0818@gmail.com>
2023-03-23 14:32:46 -0400

    Add test file

---
+++ final_project_team4_test.py
@@ -0,0 +1,83 @@
+"""
+DATS 6103 - Final Project - Team 4
+
+This script performs data preprocessing, feature engineering, model selection, and evaluation for predicting customer churn for a telecom company.
+
+The dataset 1 used is ... dataset from ... (https://).
+The dataset 2 used is ... dataset from ... (https://).
+
+The project team consists of:
+- Team Member 1
+- Team Member 2
+- Team Member 3
+- Team Member 4
+
+Usage:
+    Run the script in a Python environment with the necessary libraries installed. The script reads the dataset from the 'data' directory and outputs evaluation metrics and visualizations in the 'output' directory.
+
+Output:
+    - Metrics.csv: CSV file containing evaluation metrics for each model
+    - Feature_Importances.png: bar chart showing feature importances for the selected model
+    - Confusion_Matrix.png: confusion matrix for the selected model
+    - ROC_Curve.png: ROC curve for the selected model
+
+Note: this script requires scikit-learn version ... and pandas version ....
+"""
+#%%[markdown]
+# # Title
+# Team Member: 1, 2, 3, 4
+
+# ## Introduction
+#%%
+# Set up library
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy import stats
+import statsmodels.api as sm
+
+# Import data set
+import requests
+import io
+import zipfile
+
+#%%
+# Import data sets from online?
+# (Test) importing v-dem datasets online
+def getDFfromZip(url):
+    """ Return the data frame from a zip file
+    Parameters:
+        url(string): the url we want to unzip which contains file
+    Returns:
+        df(pandas DataFrame object): pandas dataframe objects
+    """
+    response = requests.get(url) # Send a request to download the file
+    
+    if response.status_code == 200: # Check if the request was successful
+        # Read the zip file from the response
+        with zipfile.ZipFile(io.BytesIO(response.content)) as zip_file:
+            # Find the CSV file within the zip file
+            for file in zip_file.namelist():
+                if file.endswith(".csv"):
+                    # Read the CSV file into a pandas DataFrame
+                    df = pd.read_csv(zip_file.open(file))
+                    print(df.info())                    
+    else: 
+        print("Failed to download the dataset.")
+        return 0
+    
+    return df
+
+url = "https://v-dem.net/media/datasets/V-Dem-CY-Core_csv_v13.zip"
+vdem_df = getDFfromZip(url)
+
+# %%
+vdem_df.shape
+# %%
+# (Test) data preparation and cleaning
+null_columns = vdem_df.columns[vdem_df.isnull().all()] # Find columns containing only null values
+# %%
+# (Test) 
+vdem_21century_df = vdem_df[(vdem_df['year'] >= 2000) & (vdem_df['year'] <= 2022)]
+
