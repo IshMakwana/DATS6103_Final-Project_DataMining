7e79d412ff6cb057c47d10ac1e93a0eb31a569f9
Ei Tanaka <eieio.0818@gmail.com>
2023-03-28 14:20:36 -0400

    Data Preparation from step 1 to step 4

--- final_project_team4.py
+++ final_project_team4.py
@@ -28,6 +28,7 @@ import requests
 import io
 import zipfile
 from io import StringIO
+
 #%%
 # Import data sets from online
 def getDFfromZip(url):
@@ -56,28 +57,170 @@ def getDFfromZip(url):
         return None
 
 url = "https://v-dem.net/media/datasets/V-Dem-CY-FullOthers_csv_v13.zip"
-vdem_df = getDFfromZip(url)
-vdem_df.head()
-vdem_df.shape
+VDem = getDFfromZip(url)
+VDem.head()
+VDem.shape
 
 #%%[markdown]
 # ## Data Cleaning and Prepareation
+"""
+Variables of interest (38 in totall):
+
+- country_name(str)
+- country_id(int)
+- year(int)
+
+<independent variables>
+- v2x_polyarchy (int): Electorical democracy index
+- v2x_libdem (int): Liberal democracy index
+- v2x_partipdem (int): Participatory democracy index
+- v2x_delibdem (int): Deliberative democracy index
+- v2x_egaldem (int): Egalitarian democracy index
+
+< dependent variables>
+(Education: 2 variables)
+- e_peaveduc (float): The average years of education among citizens older than 15
+- e_peedgini (float): Education inequality (Gini coefficient)
+
+(Gegraphy: 4 variables)
+- e_area (float): Area of the country (km2)
+- e_regiongeo (int): Region (geographic)
+- e_regionpol (int): Region (politico-geographic)
+- e_regionpol_6C (int): Region (politico-geographic 6-category)
+
+(Economic: 6 variables)
+- e_cow_exports (float): the total value of a country's exports
+- e_cow_imports (float): the total value of a country's imports
+- e_gdp (float): GDP
+- e_gdppc (float): GDP per capita
+- e_miinflat (float): Annual inflation rate
+- e_pop (float): Population
+
+(Natural Resources Wealth: 3 variables)
+- e_total_fuel_income_pc (float): the real value of a country's petroleum, coal, and natural gas production
+- e_total_oil_income_pc (float): the real value of a country's petroleum production
+- e_total_resources_income_pc (float): the real value of a country's petroleum, coal, natural gas, and mineral production
+
+(Infrastructure: 1 variable)
+- e_radio_n (int): the number of radio sets
+
+(Demography: 9 variables)
+- e_miferrat (float): the fertility rate
+- e_mipopula (int): the total population (in thousands
+- e_miurbani (float): the urbanization rate
+- e_miurbpop (int): the urban population (in thousands)
+- e_pefeliex (float): the life expectancy at birth among women
+- e_peinfmor (float): the infant mortality rate
+- e_pelifeex (float): the life expectancy
+- e_pematmor (float): the maternal mortality rate
+- e_wb_pop (int): the total population (in thousands)
+
+(Conflict: 5 variables)
+- e_civil_war (boolean): Was there a civil war?
+- e_miinteco (boolean): Did the country participate in an international armed conflict?
+- e_miinterc (boolean): Did the country experience an internal armed conflict?
+- e_pt_coup (int): Number of successful coup attempts in a year
+- e_pt_coup_attempts (int): Number of coups attempts in a year
+"""
+#%%
+# Step 1: Create subset containing only the columns of interest (38 variables)
+# Create a list containing the names of the variables you want to select
+variables = [
+    "country_name",
+    "country_id",
+    "year",
+    "v2x_polyarchy",
+    "v2x_libdem",
+    "v2x_partipdem",
+    "v2x_delibdem",
+    "v2x_egaldem",
+    "e_peaveduc",
+    "e_peedgini",
+    "e_area",
+    "e_regiongeo",
+    "e_regionpol",
+    "e_regionpol_6C",
+    "e_cow_exports",
+    "e_cow_imports",
+    "e_gdp",
+    "e_gdppc",
+    "e_miinflat",
+    "e_pop",
+    "e_total_fuel_income_pc",
+    "e_total_oil_income_pc",
+    "e_total_resources_income_pc",
+    "e_radio_n",
+    "e_miferrat",
+    "e_mipopula",
+    "e_miurbani",
+    "e_miurbpop",
+    "e_pefeliex",
+    "e_peinfmor",
+    "e_pelifeex",
+    "e_pematmor",
+    "e_wb_pop",
+    "e_civil_war",
+    "e_miinteco",
+    "e_miinterc",
+    "e_pt_coup",
+    "e_pt_coup_attempts"
+]
+
+# Select the desired columns from the DataFrame
+vdem_df = VDem.loc[:, variables]
+
+# Check the shape of the new DataFrame
+print(vdem_df.shape)
+vdem_df.head()
 
 #%%
-col_name = 'e_peaveduc'
-print(vdem_df.loc[vdem_df['country_name'] == 'Mexico', col_name])
+# Step 2: Create a new democracy index column at right to country_id column from 5 democracy variables
 
-col_name = 'e_area'
-print(vdem_df.loc[vdem_df['country_name'] == 'Mexico', col_name])
+# Calculate the mean of the five democracy variables for each row
+vdem_df['democracy_index'] = vdem_df[['v2x_polyarchy', 'v2x_libdem', 'v2x_partipdem', 'v2x_delibdem', 'v2x_egaldem']].mean(axis=1)
+# Move the new 'democracy_index' column to the right of the 'country_id' column
+columns = list(vdem_df.columns)
+columns.insert(columns.index("year") + 1, columns.pop(columns.index("democracy_index")))
+vdem_df = vdem_df[columns]
+# Check the shape of the new DataFrame
+print(vdem_df.shape)
+vdem_df.head()
+#%% 
+# Step 3: Create subset containing only 2000s in year column
+vdem_2000s_df = vdem_df.loc[vdem_df["year"] >= 2000]
+# Check the shape of the new DataFrame
+print(vdem_2000s_df.shape)
+vdem_2000s_df.head()
+#%%
+# Step 4: Combine the datasets by country (Combine multiple years into one and remove year column)
 
-col_name = 'e_pop'
-print(vdem_df.loc[vdem_df['country_name'] == 'Mexico', col_name])
+# Set 'country_id' and 'country_name' as a multi-level index
+vdem_2000s_df = vdem_2000s_df.set_index(['country_name', 'country_id'])
 
-col_name = 'e_radio_n'
-print(vdem_df.loc[vdem_df['country_name'] == 'Mexico', col_name])
+# Group by 'country_id' and 'country_name', and aggregate the mean
+vdem_2000s_grouped_df = vdem_2000s_df.groupby(['country_name', 'country_id']).agg("mean")
 
-col_name = 'e_pefeliex'
-print(vdem_df.loc[vdem_df['country_name'] == 'Mexico', col_name])
+# Reset the index, so 'country_name' becomes a column again
+vdem_2000s_grouped_df = vdem_2000s_grouped_df.reset_index()
+
+# Remove the 'year' column
+vdem_2000s_grouped_df = vdem_2000s_grouped_df.drop(columns=["year"])
+
+# Display the combined DataFrame
+print(vdem_2000s_grouped_df.shape)
+vdem_2000s_grouped_df.head()
+
+#%%
+# Step 5: Test 1 (If something is wrong, tell team members and go back to Step 1)
+
+#%%
+# Step 6: Data Cleaning(drop null, drop duplicates, etc.)
+
+# Step 7: change variable name
+
+# Step 8: check data type (and change if necessary)
+
+# Step 9: Test 2
 #%%[markdown]
 # ## EDA
 
