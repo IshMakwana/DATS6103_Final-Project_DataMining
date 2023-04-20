b410f7be532c2c3fea63f271ac46e9977335a25d
IshMakwana <ishmakwana@gmail.com>
2023-04-20 17:58:44 -0400

    Data Cleaning/modify and variable, minmax

--- final_project_team4.py
+++ final_project_team4.py
@@ -58,114 +58,115 @@ from scipy.stats import pearsonr
 import statsmodels.api as sm
 
 
-#%% 
-# Import data sets from Github repository
-# The code creates a DataFrame that includes 38 variables of interest. The names of the variables are stored in a list called variables. The code selects the desired columns from the original DataFrame using the .loc[] method and stores the resulting subset in a new DataFrame called WorldBank_df. 
-import pandas as pd
-
-url = 'https://raw.githubusercontent.com/IshMakwana/DATS6103_Final-Project_DataMining/main/dataset/vdem_worldBank.csv'
-WorldBank_df= pd.read_csv(url)
-print(WorldBank_df.head(5))
-
 #%%
-# Import data sets from online
-# The code creates a subset of the VDem DataFrame that includes 38 variables of interest. The names of the variables are stored in a list called variables. The code selects the desired columns from the original DataFrame using the .loc[] method and stores the resulting subset in a new DataFrame called vdem_df. 
+#Import data sets from online
+#The code creates a subset of the VDem DataFrame that includes 38 variables of interest. The names of the variables are stored in a list called variables. The code selects the desired columns from the original DataFrame using the .loc[] method and stores the resulting subset in a new DataFrame called vdem_df. 
 
 
-# def getDFfromZip(url):
-#     """ Return the data frame from a csv file in a zip file
-#     Parameters:
-#         url(str): url of the zip file
-#     Returns:
-#         df(pandas.DataFrame): data frame of the csv file
-#     """
-#     response = requests.get(url) # Send a request to download the file
+def getDFfromZip(url):
+    """ Return the data frame from a csv file in a zip file
+    Parameters:
+        url(str): url of the zip file
+    Returns:
+        df(pandas.DataFrame): data frame of the csv file
+    """
+    response = requests.get(url) # Send a request to download the file
     
-#     if response.status_code == 200: # Check if the request was successful
-#         # Read the zip file from the response
-#         with zipfile.ZipFile(io.BytesIO(response.content)) as zip_file:
-#             # Find the CSV file within the zip file
-#             for file in zip_file.namelist():
-#                 if file.endswith(".csv"):
-#                     # Read the CSV file into a pandas DataFrame
-#                     df = pd.read_csv(zip_file.open(file))                    
-#                     print(df.shape)      
-#                     return df
-#             # If the CSV file was not found, return None
-#             return None
-#     else: 
-#         print("Failed to download the dataset.")
-#         return None
+    if response.status_code == 200: # Check if the request was successful
+        # Read the zip file from the response
+        with zipfile.ZipFile(io.BytesIO(response.content)) as zip_file:
+            # Find the CSV file within the zip file
+            for file in zip_file.namelist():
+                if file.endswith(".csv"):
+                    # Read the CSV file into a pandas DataFrame
+                    df = pd.read_csv(zip_file.open(file))                    
+                    print(df.shape)      
+                    return df
+            # If the CSV file was not found, return None
+            return None
+    else: 
+        print("Failed to download the dataset.")
+        return None
 
-# url = "https://v-dem.net/media/datasets/V-Dem-CY-FullOthers_csv_v13.zip"
-# VDem = getDFfromZip(url)
-# VDem.head()
-# VDem.shape
-
-#%%[markdown]
-# ## Data Pre-Processing
-"""
-Variables of interest (38 in totall): original variables in the dataset 
-
-- country_name(str)
-- country_id(int)
-- year(int)
-
-<independent variables>
-- v2x_polyarchy (int): Electorical democracy index
-- v2x_libdem (int): Liberal democracy index
-- v2x_partipdem (int): Participatory democracy index
-- v2x_delibdem (int): Deliberative democracy index
-- v2x_egaldem (int): Egalitarian democracy index
-
-< dependent variables>
-(Education: 2 variables)
-- e_peaveduc (float): The average years of education among citizens older than 15
-- e_peedgini (float): Education inequality (Gini coefficient)
-
-(Geography: 4 variables)
-- e_area (float): Area of the country (km2)
-- e_regiongeo (int): Region (geographic)
-- e_regionpol (int): Region (politico-geographic)
-- e_regionpol_6C (int): Region (politico-geographic 6-category)
-
-(Economic: 6 variables)
-- e_cow_exports (float): the total value of a country's exports
-- e_cow_imports (float): the total value of a country's imports
-- e_gdp (float): GDP
-- e_gdppc (float): GDP per capita
-- e_miinflat (float): Annual inflation rate
-- e_pop (float): Population
-
-(Natural Resources Wealth: 3 variables)
-- e_total_fuel_income_pc (float): the real value of a country's petroleum, coal, and natural gas production
-- e_total_oil_income_pc (float): the real value of a country's petroleum production
-- e_total_resources_income_pc (float): the real value of a country's petroleum, coal, natural gas, and mineral production
-
-(Infrastructure: 1 variable)
-- e_radio_n (int): the number of radio sets
-
-(Demography: 9 variables)
-- e_miferrat (float): the fertility rate
-- e_mipopula (int): the total population (in thousands
-- e_miurbani (float): the urbanization rate
-- e_miurbpop (int): the urban population (in thousands)
-- e_pefeliex (float): the life expectancy at birth among women
-- e_peinfmor (float): the infant mortality rate
-- e_pelifeex (float): the life expectancy
-- e_pematmor (float): the maternal mortality rate
-- e_wb_pop (int): the total population (in thousands)
-
-(Conflict: 5 variables)
-- e_civil_war (boolean): Was there a civil war?
-- e_miinteco (boolean): Did the country participate in an international armed conflict?
-- e_miinterc (boolean): Did the country experience an internal armed conflict?
-- e_pt_coup (int): Number of successful coup attempts in a year
-- e_pt_coup_attempts (int): Number of coups attempts in a year
-"""
+url = "https://v-dem.net/media/datasets/V-Dem-CY-FullOthers_csv_v13.zip"
+VDem = getDFfromZip(url)
+VDem.head()
+VDem.shape
 
 #%%
-# Create a list containing the names of the variables we are interested in
+""" Variables of Interest(19):
+Infrastracture
+- Access_to_clean_fuels_and_technologies_for_cooking_(%_of_population)
+- Mobile_cellular_subscriptions_(per_100_people)
+
+Demographic
+- Adolescent_fertility_rate_(births_per_1,000_women_ages_15-19)
+- Fertility_rate,_total_(births_per_woman)
+- Life_expectancy_at_birth,_total_(years)
+- Mortality_rate,_under-5_(per_1,000_live_births)
+- Net_migration
+- Population_growth_(annual_%)
+
+Environment
+- CO2_emissions_(metric_tons_per_capita)
+
+Health
+- Immunization,_measles_(%_of_children_ages_12-23_months)
+- Prevalence_of_HIV,_total_(%_of_population_ages_15-49)
+
+Education
+- School_enrollment,_primary_(%_gross)'], dtype=object)
+
+Economy
+- Agriculture,_forestry,_and_fishing,_value_added_(%_of_GDP)
+- Exports_of_goods_and_services_(%_of_GDP)
+- Foreign_direct_investment,_net_inflows_(BoP,_current_US$)
+- GDP_(current_US$)', 'GDP_growth_(annual_%)
+- GNI_per_capita,_Atlas_method_(current_US$)
+- Imports_of_goods_and_services_(%_of_GDP)
+"""
+
+#%% Import data seets from local (World Bank; World Development Indicators) and reformat
+url2 = "https://raw.githubusercontent.com/IshMakwana/DATS6103_Final-Project_DataMining/main/dataset/WorldBank.csv"
+
+WorldBank_df = pd.read_csv(url2)
+WorldBank_df.head()
+
+#%%
+# create a dictionary for c_id
+c_id_dict = WorldBank_df.set_index('c_id')['CountryName'].to_dict()
+droped_WorldBank_df = WorldBank_df.drop(['CountryName', 'SeriesName'], axis=1)
+
+# Reshape the data frame
+new_WB_df = pd.melt(droped_WorldBank_df, id_vars=['c_id', 's_id'], var_name='year', value_name='value')
+new_WB_df['s_id'] = 's_id.' + new_WB_df['s_id'].astype(str)
+new_WB_df = new_WB_df.pivot(index=['c_id', 'year'], columns='s_id', values='value').reset_index()
+
+# reorder columns
+new_order = ['c_id', 'year', 's_id.1', 's_id.2', 's_id.3', 's_id.4', 's_id.5', 's_id.6', 's_id.7', 's_id.8', 's_id.9', 's_id.10', 's_id.11', 's_id.12', 's_id.13', 's_id.14', 's_id.15', 's_id.16', 's_id.17', 's_id.18', 's_id.19']
+new_WB_df = new_WB_df.reindex(columns=new_order)
+
+# change column names based on s_id_dict
+new_colnames = {'c_id': 'c_id', 'year': 'year',
+            's_id.1': 'AccessToCleanCooking', 's_id.2': 'AdolescentFertility',
+            's_id.3': 'AgriForestFishValueAdded', 's_id.4': 'CO2Emissions',
+            's_id.5': 'ExportsOfGoodsAndServices', 's_id.6': 'FertilityRate',
+            's_id.7': 'ForeignDirectInvestment', 's_id.8': 'GDP',
+            's_id.9': 'GDPGrowth', 's_id.10': 'GNIPerCapita',
+            's_id.11': 'MeaslesImmunization', 's_id.12': 'ImportsOfGoodsAndServices',
+            's_id.13': 'LifeExpectancy', 's_id.14': 'MobileSubscriptions',
+            's_id.15': 'Under5Mortality', 's_id.16': 'NetMigration',
+            's_id.17': 'PopulationGrowth', 's_id.18': 'HIVPrevalence',
+            's_id.19': 'PrimarySchoolEnrollment'}
+new_WB_df = new_WB_df.rename(columns=new_colnames)
+
+# remove "yr" from "year" column
+new_WB_df['year'] = new_WB_df['year'].str.replace('yr', '')
+# rename "values of c_id" column" bsed on c_id_dict
+new_WB_df['c_id'] = new_WB_df['c_id'].map(c_id_dict)
+new_WB_df = new_WB_df.rename(columns={'c_id': 'country_name'})
+
+#%% Create a list containing the names of the variables we are interested in
 variables = [
     "country_name",
     "country_id",
@@ -175,44 +176,13 @@ variables = [
     "v2x_partipdem",
     "v2x_delibdem",
     "v2x_egaldem",
-    "e_peaveduc",
-    "e_peedgini",
-    "e_area",
-    "e_regiongeo",
-    "e_regionpol",
     "e_regionpol_6C",
-    "e_cow_exports",
-    "e_cow_imports",
-    "e_gdp",
-    "e_gdppc",
-    "e_miinflat",
-    "e_pop",
-    "e_total_fuel_income_pc",
-    "e_total_oil_income_pc",
-    "e_total_resources_income_pc",
-    "e_radio_n",
-    "e_miferrat",
-    "e_mipopula",
-    "e_miurbani",
-    "e_miurbpop",
-    "e_pefeliex",
-    "e_peinfmor",
-    "e_pelifeex",
-    "e_pematmor",
-    "e_wb_pop",
-    "e_civil_war",
-    "e_miinteco",
-    "e_miinterc",
-    "e_pt_coup",
-    "e_pt_coup_attempts",
-    "v2smpardom",
-
 ]
 vdem_df = VDem.loc[:, variables]
 print(vdem_df.shape)
 vdem_df.head()
-#%%
-# Create a new democracy index column at right to country_id column from 5 democracy variables
+
+#%% Create a new democracy index column at right to country_id column from 5 democracy variables
 # Calculate the mean of the five democracy variables for each row
 vdem_df['democracy_index'] = vdem_df[['v2x_polyarchy', 'v2x_libdem', 'v2x_partipdem', 
                                     'v2x_delibdem', 'v2x_egaldem']].mean(axis=1)
@@ -224,6 +194,132 @@ vdem_df = vdem_df[columns]
 
 print(vdem_df.shape)
 vdem_df.head()
+
+#%% Create subset containing only 2000s in year column
+# Create a new DataFrame containing only the rows from 2000 onwards
+vdem_2000s_df = vdem_df.loc[vdem_df["year"] >= 2000]
+# Check the shape of the new DataFrame
+print(vdem_2000s_df.shape)
+vdem_2000s_df.head()
+
+#%% Count the number of countries which are included in both vdem_2000s_df and new_WB_df
+number_of_countries = set(vdem_2000s_df["country_name"]).intersection(set(new_WB_df["country_name"]))
+number_of_countries
+
+#%% Merge the two dataframes
+vdem_2000s_df['country_name'] = vdem_2000s_df['country_name'].replace('United States of America', 'United States')
+vdem_2000s_df.info()
+new_WB_df.info()
+new_WB_df['year'] = new_WB_df['year'].astype(int)
+
+new_df = pd.merge(vdem_2000s_df, new_WB_df, on=["country_name", "year"], how="inner")
+# %% Drop the countries where don't have 22 years of data
+counts = new_df['country_name'].value_counts()
+not_22 = counts[counts != 22]
+countries = not_22.index.tolist() # only south sudan has less than 22 years of data
+new_df = new_df[~new_df['country_name'].isin(countries)]
+
+#%% Check and Handle Missing Values
+print(new_df.isnull().sum())
+
+# Fill null values by meadian among same country name.
+# If there is no value among same country name, fill them by median among same political region.
+# I chose median because the distribution of almost all variables was not normal distribute.
+def fillna(df):
+    # Fill NaN values by median among the same country name
+    df = df.groupby('country_name').apply(lambda x: x.fillna(x.median()))
+
+    # Fill NaN values by median among the same political region if country median is not available
+    df = df.groupby('e_regionpol_6C').apply(lambda x: x.fillna(x.median()))
+
+    # Reformat the structure
+    df = df.reset_index(drop=True)
+
+    return df
+
+#%% Fill missing values
+new_df_2 = fillna(new_df)
+new_df_2.isnull().sum()
+print(new_df_2.shape)
+#%% Export the dataset as a new CSV file
+#new_df_2.to_csv('dataset/vdem_worldBank.csv', index=False)
+#%% EDA
+vdem_worldBank_df = pd.read_csv('dataset/vdem_worldBank.csv')
+#%% Basic Information (country time series)
+vdem_worldBank_df.head()
+#%% Data Types, Data Structure, number of rows and columns, and missing values (country time series)
+vdem_worldBank_df.info()
+#%% Descriptive Statistics (country time series)
+vdem_worldBank_df.describe()
+#%% Columns (country time series)
+vdem_worldBank_df.columns
+#%% Countries (country time series)
+vdem_worldBank_df['country_name'].unique() 
+#%% Create a new dataframe aggregating the mean of the democracy index for each country
+vdem_worldBank_grouped_country = vdem_worldBank_df.groupby('country_name').mean()
+vdem_worldBank_grouped_country.reset_index(inplace=True)
+#%% Basic Information (grouped by country)
+vdem_worldBank_grouped_country.head()
+#%% Data Types, number of rows and columns, and missing values (grouped by country)
+vdem_worldBank_grouped_country.info()
+#%% Data Structure (grouped by country)
+vdem_worldBank_grouped_country.shape
+#%% Missing Values (grouped by country)
+vdem_worldBank_grouped_country.isnull().sum()
+#%% Descriptive Statistics (grouped by country)
+vdem_worldBank_grouped_country.describe()
+#%% Create a new dataframe aggregating the mean of the democracy index for each political region with time series
+vdem_worldBank_poli_region = vdem_worldBank_df.groupby(['e_regionpol_6C', 'year']).mean()
+vdem_worldBank_poli_region.reset_index(inplace=True)
+#%% Basic Information (grouped by political region with time series)
+vdem_worldBank_poli_region.head()
+#%% Data Types, number of rows and columns, and missing values (grouped by political region with time series)
+vdem_worldBank_poli_region.info()
+#%% Data Structure (grouped by political region with time series)
+vdem_worldBank_poli_region.shape
+#%% Descriptive Statistics (grouped by political region with time series)
+vdem_worldBank_poli_region.describe()
+#%% Create a new dataframe aggregating the mean of the democracy index for each political region
+vdem_worldBank_poli_region_grouped = vdem_worldBank_df.groupby('e_regionpol_6C').mean()
+vdem_worldBank_poli_region_grouped.reset_index(inplace=True)
+#%% Basic Information (grouped by political region)
+vdem_worldBank_poli_region_grouped.head()
+#%% Data Types, number of rows and columns, and missing values (grouped by political region)
+vdem_worldBank_poli_region_grouped.info()
+#%% Data Structure (grouped by political region)
+vdem_worldBank_poli_region_grouped.shape
+#%% Descriptive Statistics (grouped by political region)
+vdem_worldBank_poli_region_grouped.describe()
+#%% Add the columns for political region dataframe
+poli_region_dict = {1: 'Eastern_Europe_and_Central_Asia', 
+                    2: 'Latin_America_and_the_Caribbean', 
+                    3: 'Middle_East_and_North_Africa', 
+                    4:'Sub-Saharan_Africa', 
+                    5: 'Western_Europe_North_America_and_Oceania', 
+                    6: 'Asia_and_Pacific'}
+
+vdem_worldBank_df['political_region'] = vdem_worldBank_df['e_regionpol_6C'].map(poli_region_dict)
+vdem_worldBank_grouped_country['political_region'] = vdem_worldBank_grouped_country['e_regionpol_6C'].map(poli_region_dict)
+vdem_worldBank_poli_region['political_region'] = vdem_worldBank_poli_region['e_regionpol_6C'].map(poli_region_dict)
+vdem_worldBank_poli_region_grouped['political_region'] = vdem_worldBank_poli_region_grouped['e_regionpol_6C'].map(poli_region_dict)
+
+#%% Max, Min and Mean of Democracy Index in 2000s (mean of 2000-2021)
+max_demo_index = vdem_worldBank_grouped_country['democracy_index'].max()
+min_demo_index = vdem_worldBank_grouped_country['democracy_index'].min()
+mean_demo_index = vdem_worldBank_grouped_country['democracy_index'].mean()
+
+closest_country = vdem_worldBank_grouped_country.loc[(vdem_worldBank_grouped_country['democracy_index'] - mean_demo_index).abs().argsort()[0], 'country_name']
+
+print(f'Max democracy index: {round(max_demo_index, 2)}, Country: {vdem_worldBank_grouped_country[vdem_worldBank_grouped_country["democracy_index"] == max_demo_index]["country_name"].values[0]}')
+print(f'Min democracy index: {round(min_demo_index, 2)}, Country: {vdem_worldBank_grouped_country[vdem_worldBank_grouped_country["democracy_index"] == min_demo_index]["country_name"].values[0]}')
+print(f'Mean democracy index: {round(mean_demo_index, 2)}, Country: {closest_country}')
+#%% Outliers
+num_cols = ['democracy_index','v2x_polyarchy', 'v2x_libdem',          
+            'v2x_partipdem', 'v2x_delibdem','v2x_egaldem', 'e_regionpol_6C', 'AccessToCleanCooking','AdolescentFertility', 'AgriForestFishValueAdded', 'CO2Emissions','ExportsOfGoodsAndServices', 'FertilityRate','ForeignDirectInvestment','GDP', 'GDPGrowth', 'GNIPerCapita', 'MeaslesImmunization','ImportsOfGoodsAndServices', 'LifeExpectancy', 'MobileSubscriptions','Under5Mortality', 'NetMigration', 'PopulationGrowth', 'HIVPrevalence','PrimarySchoolEnrollment']
+# Create a new DataFrame containing only the numeric columns
+df_subset = vdem_worldBank_df[num_cols]
+# Create a boxplot for each numeric column
+sns.boxplot(data=df_subset, orient="h", palette="Set2")
 #%% 
 # Create subset containing only 2000s in year column
 # Create a new DataFrame containing only the rows from 2000 onwards
